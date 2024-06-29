27f095b1c3ae725ebcf9b13588fc22142a1a081b
anthonybellon <anthonybellonparis@gmail.com>
2024-06-29 18:58:04 +0200

    feat: first version of template

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customConfig: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/app/pages/$1',
+    '^@styles/(.*)$': '<rootDir>/src/app/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
+
+export default createJestConfig(customConfig);
